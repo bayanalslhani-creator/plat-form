7081b2a3e5f072531a1599e4321d28cc7d5cfd02
Keep going <bayanalslhani@gmail.com>
2026-08-13 13:35:13 +0000

    File changes

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -124,7 +124,7 @@ function MediaFrame({ project, animated }) {
     <img
       src={project.img}
       alt={project.title}
-      className="h-full w-full bg-black object-contain"
+      className="h-full w-full object-cover"
     />
   );
 }
