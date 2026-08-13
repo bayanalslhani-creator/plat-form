a17a11f61ad9643d5d22c020968022851c7e3728
Keep going <bayanalslhani@gmail.com>
2026-08-13 13:55:08 +0000

    File changes

--- src/pages/JoinUs.jsx
+++ src/pages/JoinUs.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import { base44 } from "@/api/base44Client";
 import { ArrowRight, Check, Loader2 } from "lucide-react";
 
-import LogoMark from "@/components/LogoMark";
+const LOGO = "https://media.base44.com/images/public/6a6f7ad42956b021bd3930bf/e53489781_.jpg";
 
 const INTERESTS = [
   "التصوير الفوتوغرافي",
@@ -50,7 +50,7 @@ export default function JoinUs() {
 
       <div className="relative mx-auto max-w-3xl px-6">
         <Link to="/" className="inline-flex items-center">
-          <LogoMark className="h-20 w-auto" />
+          <img src={LOGO} alt="Plat Form Media" className="h-20 w-auto rounded-md mix-blend-screen" />
         </Link>
 
         <motion.div
